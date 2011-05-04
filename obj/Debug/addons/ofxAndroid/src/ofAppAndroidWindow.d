0a814896ceae50d8448f966091dd87fa92195b8e
Daniel T <tsadok@bigfoot.com>
2011-05-04 01:55:27 -0400

    initial commit - got 3D model on camra overlay!

---
+++ obj/Debug/addons/ofxAndroid/src/ofAppAndroidWindow.d
@@ -0,0 +1,255 @@
+obj/Debug/addons/ofxAndroid/src/ofAppAndroidWindow.d \
+ obj/Debug/addons/ofxAndroid/src/ofAppAndroidWindow.o: \
+ ../../../addons/ofxAndroid/src/ofAppAndroidWindow.cpp \
+ ../../../addons/ofxAndroid/src/ofAppAndroidWindow.h \
+ ../../../libs/openFrameworks//app/ofAppBaseWindow.h \
+ ../../../libs/openFrameworks//types/ofPoint.h \
+ ../../../libs/openFrameworks//math/ofVec3f.h \
+ ../../../libs/openFrameworks//math/ofVec2f.h \
+ ../../../libs/openFrameworks//utils/ofConstants.h \
+ ../../../libs/glu/include_android/glu.h \
+ /Developer/android-ndk-r5b/sources/cxx-stl/gnu-libstdc++/include/cstdlib \
+ /Developer/android-ndk-r5b/sources/cxx-stl/gnu-libstdc++/include/cstdio \
+ /Developer/android-ndk-r5b/sources/cxx-stl/gnu-libstdc++/include/cstdarg \
+ /Developer/android-ndk-r5b/sources/cxx-stl/gnu-libstdc++/include/cmath \
+ /Developer/android-ndk-r5b/sources/cxx-stl/gnu-libstdc++/include/ctime \
+ /Developer/android-ndk-r5b/sources/cxx-stl/gnu-libstdc++/include/string \
+ /Developer/android-ndk-r5b/sources/cxx-stl/gnu-libstdc++/include/iostream \
+ /Developer/android-ndk-r5b/sources/cxx-stl/gnu-libstdc++/include/vector \
+ /Developer/android-ndk-r5b/sources/cxx-stl/gnu-libstdc++/include/cstring \
+ /Developer/android-ndk-r5b/sources/cxx-stl/gnu-libstdc++/include/sstream \
+ /Developer/android-ndk-r5b/sources/cxx-stl/gnu-libstdc++/include/iomanip \
+ /Developer/android-ndk-r5b/sources/cxx-stl/gnu-libstdc++/include/fstream \
+ ../../../libs/openFrameworks//math/ofVec4f.h \
+ ../../../libs/openFrameworks//graphics/ofGraphics.h \
+ ../../../libs/openFrameworks//types/ofColor.h \
+ ../../../libs/openFrameworks//math/ofMatrix4x4.h \
+ ../../../libs/openFrameworks//math/ofQuaternion.h \
+ ../../../libs/openFrameworks//types/ofRectangle.h \
+ ../../../libs/openFrameworks//types/ofTypes.h \
+ ../../../libs/poco/include/Poco/Mutex.h \
+ ../../../libs/poco/include/Poco/Foundation.h \
+ ../../../libs/poco/include/Poco/Config.h \
+ ../../../libs/poco/include/Poco/Platform.h \
+ ../../../libs/poco/include/Poco/Platform_POSIX.h \
+ ../../../libs/poco/include/Poco/Bugcheck.h \
+ ../../../libs/poco/include/Poco/Types.h \
+ ../../../libs/poco/include/Poco/Exception.h \
+ /Developer/android-ndk-r5b/sources/cxx-stl/gnu-libstdc++/include/stdexcept \
+ ../../../libs/poco/include/Poco/ScopedLock.h \
+ ../../../libs/poco/include/Poco/Mutex_POSIX.h \
+ ../../../libs/openFrameworks//types/ofBaseTypes.h \
+ ../../../libs/openFrameworks//3d/ofMesh.h \
+ ../../../libs/openFrameworks//utils/ofUtils.h \
+ /Developer/android-ndk-r5b/sources/cxx-stl/gnu-libstdc++/include/bitset \
+ ../../../libs/openFrameworks//utils/ofLog.h \
+ ../../../libs/openFrameworks//gl/ofGLUtils.h \
+ ../../../libs/openFrameworks//app/ofAppRunner.h \
+ ../../../addons/ofxAndroid/src/ofxAndroidApp.h \
+ ../../../libs/openFrameworks//app/ofBaseApp.h \
+ ../../../libs/openFrameworks//events/ofEvents.h \
+ ../../../libs/openFrameworks//events/ofEventUtils.h \
+ ../../../libs/poco/include/Poco/FIFOEvent.h \
+ ../../../libs/poco/include/Poco/AbstractEvent.h \
+ ../../../libs/poco/include/Poco/SingletonHolder.h \
+ ../../../libs/poco/include/Poco/SharedPtr.h \
+ ../../../libs/poco/include/Poco/AtomicCounter.h \
+ /Developer/android-ndk-r5b/sources/cxx-stl/gnu-libstdc++/include/algorithm \
+ ../../../libs/poco/include/Poco/ActiveResult.h \
+ ../../../libs/poco/include/Poco/Event.h \
+ ../../../libs/poco/include/Poco/Event_POSIX.h \
+ ../../../libs/poco/include/Poco/RefCountedObject.h \
+ ../../../libs/poco/include/Poco/ActiveMethod.h \
+ ../../../libs/poco/include/Poco/ActiveRunnable.h \
+ ../../../libs/poco/include/Poco/Runnable.h \
+ ../../../libs/poco/include/Poco/AutoPtr.h \
+ ../../../libs/poco/include/Poco/ActiveStarter.h \
+ ../../../libs/poco/include/Poco/ThreadPool.h \
+ ../../../libs/poco/include/Poco/Thread.h \
+ ../../../libs/poco/include/Poco/Thread_POSIX.h \
+ ../../../libs/poco/include/Poco/SignalHandler.h \
+ ../../../libs/poco/include/Poco/FIFOStrategy.h \
+ ../../../libs/poco/include/Poco/NotificationStrategy.h \
+ /Developer/android-ndk-r5b/sources/cxx-stl/gnu-libstdc++/include/map \
+ /Developer/android-ndk-r5b/sources/cxx-stl/gnu-libstdc++/include/list \
+ /Developer/android-ndk-r5b/sources/cxx-stl/gnu-libstdc++/include/memory \
+ ../../../libs/poco/include/Poco/AbstractDelegate.h \
+ ../../../libs/poco/include/Poco/CompareFunctions.h \
+ /Developer/android-ndk-r5b/sources/cxx-stl/gnu-libstdc++/include/functional \
+ ../../../libs/poco/include/Poco/Delegate.h \
+ ../../../libs/poco/include/Poco/FunctionDelegate.h \
+ ../../../libs/poco/include/Poco/Expire.h \
+ ../../../libs/poco/include/Poco/Timestamp.h \
+ ../../../addons/ofxAndroid/src/ofxAndroidUtils.h \
+ ../../../libs/openFrameworks//sound/ofSoundStream.h \
+ ../../../addons/ofxAndroid/src/ofxAndroidSoundStream.h \
+ ../../../libs/openFrameworks//sound/ofBaseSoundStream.h \
+ ../../../addons/ofxAccelerometer/src/ofxAccelerometer.h
+
+../../../addons/ofxAndroid/src/ofAppAndroidWindow.h:
+
+../../../libs/openFrameworks//app/ofAppBaseWindow.h:
+
+../../../libs/openFrameworks//types/ofPoint.h:
+
+../../../libs/openFrameworks//math/ofVec3f.h:
+
+../../../libs/openFrameworks//math/ofVec2f.h:
+
+../../../libs/openFrameworks//utils/ofConstants.h:
+
+../../../libs/glu/include_android/glu.h:
+
+/Developer/android-ndk-r5b/sources/cxx-stl/gnu-libstdc++/include/cstdlib:
+
+/Developer/android-ndk-r5b/sources/cxx-stl/gnu-libstdc++/include/cstdio:
+
+/Developer/android-ndk-r5b/sources/cxx-stl/gnu-libstdc++/include/cstdarg:
+
+/Developer/android-ndk-r5b/sources/cxx-stl/gnu-libstdc++/include/cmath:
+
+/Developer/android-ndk-r5b/sources/cxx-stl/gnu-libstdc++/include/ctime:
+
+/Developer/android-ndk-r5b/sources/cxx-stl/gnu-libstdc++/include/string:
+
+/Developer/android-ndk-r5b/sources/cxx-stl/gnu-libstdc++/include/iostream:
+
+/Developer/android-ndk-r5b/sources/cxx-stl/gnu-libstdc++/include/vector:
+
+/Developer/android-ndk-r5b/sources/cxx-stl/gnu-libstdc++/include/cstring:
+
+/Developer/android-ndk-r5b/sources/cxx-stl/gnu-libstdc++/include/sstream:
+
+/Developer/android-ndk-r5b/sources/cxx-stl/gnu-libstdc++/include/iomanip:
+
+/Developer/android-ndk-r5b/sources/cxx-stl/gnu-libstdc++/include/fstream:
+
+../../../libs/openFrameworks//math/ofVec4f.h:
+
+../../../libs/openFrameworks//graphics/ofGraphics.h:
+
+../../../libs/openFrameworks//types/ofColor.h:
+
+../../../libs/openFrameworks//math/ofMatrix4x4.h:
+
+../../../libs/openFrameworks//math/ofQuaternion.h:
+
+../../../libs/openFrameworks//types/ofRectangle.h:
+
+../../../libs/openFrameworks//types/ofTypes.h:
+
+../../../libs/poco/include/Poco/Mutex.h:
+
+../../../libs/poco/include/Poco/Foundation.h:
+
+../../../libs/poco/include/Poco/Config.h:
+
+../../../libs/poco/include/Poco/Platform.h:
+
+../../../libs/poco/include/Poco/Platform_POSIX.h:
+
+../../../libs/poco/include/Poco/Bugcheck.h:
+
+../../../libs/poco/include/Poco/Types.h:
+
+../../../libs/poco/include/Poco/Exception.h:
+
+/Developer/android-ndk-r5b/sources/cxx-stl/gnu-libstdc++/include/stdexcept:
+
+../../../libs/poco/include/Poco/ScopedLock.h:
+
+../../../libs/poco/include/Poco/Mutex_POSIX.h:
+
+../../../libs/openFrameworks//types/ofBaseTypes.h:
+
+../../../libs/openFrameworks//3d/ofMesh.h:
+
+../../../libs/openFrameworks//utils/ofUtils.h:
+
+/Developer/android-ndk-r5b/sources/cxx-stl/gnu-libstdc++/include/bitset:
+
+../../../libs/openFrameworks//utils/ofLog.h:
+
+../../../libs/openFrameworks//gl/ofGLUtils.h:
+
+../../../libs/openFrameworks//app/ofAppRunner.h:
+
+../../../addons/ofxAndroid/src/ofxAndroidApp.h:
+
+../../../libs/openFrameworks//app/ofBaseApp.h:
+
+../../../libs/openFrameworks//events/ofEvents.h:
+
+../../../libs/openFrameworks//events/ofEventUtils.h:
+
+../../../libs/poco/include/Poco/FIFOEvent.h:
+
+../../../libs/poco/include/Poco/AbstractEvent.h:
+
+../../../libs/poco/include/Poco/SingletonHolder.h:
+
+../../../libs/poco/include/Poco/SharedPtr.h:
+
+../../../libs/poco/include/Poco/AtomicCounter.h:
+
+/Developer/android-ndk-r5b/sources/cxx-stl/gnu-libstdc++/include/algorithm:
+
+../../../libs/poco/include/Poco/ActiveResult.h:
+
+../../../libs/poco/include/Poco/Event.h:
+
+../../../libs/poco/include/Poco/Event_POSIX.h:
+
+../../../libs/poco/include/Poco/RefCountedObject.h:
+
+../../../libs/poco/include/Poco/ActiveMethod.h:
+
+../../../libs/poco/include/Poco/ActiveRunnable.h:
+
+../../../libs/poco/include/Poco/Runnable.h:
+
+../../../libs/poco/include/Poco/AutoPtr.h:
+
+../../../libs/poco/include/Poco/ActiveStarter.h:
+
+../../../libs/poco/include/Poco/ThreadPool.h:
+
+../../../libs/poco/include/Poco/Thread.h:
+
+../../../libs/poco/include/Poco/Thread_POSIX.h:
+
+../../../libs/poco/include/Poco/SignalHandler.h:
+
+../../../libs/poco/include/Poco/FIFOStrategy.h:
+
+../../../libs/poco/include/Poco/NotificationStrategy.h:
+
+/Developer/android-ndk-r5b/sources/cxx-stl/gnu-libstdc++/include/map:
+
+/Developer/android-ndk-r5b/sources/cxx-stl/gnu-libstdc++/include/list:
+
+/Developer/android-ndk-r5b/sources/cxx-stl/gnu-libstdc++/include/memory:
+
+../../../libs/poco/include/Poco/AbstractDelegate.h:
+
+../../../libs/poco/include/Poco/CompareFunctions.h:
+
+/Developer/android-ndk-r5b/sources/cxx-stl/gnu-libstdc++/include/functional:
+
+../../../libs/poco/include/Poco/Delegate.h:
+
+../../../libs/poco/include/Poco/FunctionDelegate.h:
+
+../../../libs/poco/include/Poco/Expire.h:
+
+../../../libs/poco/include/Poco/Timestamp.h:
+
+../../../addons/ofxAndroid/src/ofxAndroidUtils.h:
+
+../../../libs/openFrameworks//sound/ofSoundStream.h:
+
+../../../addons/ofxAndroid/src/ofxAndroidSoundStream.h:
+
+../../../libs/openFrameworks//sound/ofBaseSoundStream.h:
+
+../../../addons/ofxAccelerometer/src/ofxAccelerometer.h:
---
+++ src/testApp.h
@@ -0,0 +1,48 @@
+#ifndef _TEST_APP
+#define _TEST_APP
+
+#include "ofMain.h"
+#include "ofxAssimpModelLoader.h"
+#include "ofVboMesh.h"
+#include "ofx3DModelLoader.h"
+
+//http://innovator.samsungmobile.com/galaxyTab.do
+#define TAB_DENSITY 240
+#define TAB_XDPI 168
+#define TAB_YDPI 168
+
+class testApp : public ofBaseApp{
+
+	public:
+		void setup();
+		void update();
+		void draw();
+		
+		void keyPressed(int key);
+		void keyReleased(int key);
+		void mouseMoved(int x, int y );
+		void mouseDragged(int x, int y, int button);
+		void mousePressed(int x, int y, int button);
+		void mouseReleased(int x, int y, int button);
+		void windowResized(int w, int h);
+		void dragEvent(ofDragInfo dragInfo);
+		void gotMessage(ofMessage msg);		
+    
+		bool bAnimate;
+		bool bAnimateMouse;
+		float animationTime;
+        ofxAssimpModelLoader model;
+
+        ofVideoGrabber grabber;
+
+        ofVboMesh mesh;
+        ofPoint position;
+        float normScale;
+        ofPoint scale;
+        ofPoint sceneCenter;
+        ofMaterial material;
+        ofTexture tex;
+        ofLight	light;
+};
+
+#endif
